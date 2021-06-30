b35a889a1243e474c8104a77a38213b93f14d39a
spiCkyyy1 <hassam_waheed@outlook.com>
2021-06-30 15:26:45 +0500

    cron job work done

---
+++ app/Console/Commands/RemoveIncompleteApplicants.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Mail\RemoveApplicants;
+use App\Models\JobSeeker;
+use App\Models\ReadinessAssessment;
+use Illuminate\Console\Command;
+use DB;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Storage;
+
+class RemoveIncompleteApplicants extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'remove:jobseekers';
+    private $totalCompetencies = 11;
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Remove job seekers who have got their application accepted but haven"t given the assessment test.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $jobSeekers = JobSeeker::where('status', 1)->get();
+
+        foreach ($jobSeekers as $jobSeeker) {
+
+            if ($jobSeeker->answered_competencies != $this->totalCompetencies) {
+                Mail::to($jobSeeker->email)->send(new RemoveApplicants());
+                JobSeeker::find($jobSeeker->id)->delete();
+                ReadinessAssessment::where('job_seeker_id', $jobSeeker->id)->delete();
+                Storage::deleteDirectory('public/'.$jobSeeker->id);
+            }
+        }
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\RemoveIncompleteApplicants;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        RemoveIncompleteApplicants::class,
     ];
 
     /**
@@ -24,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('remove:jobseekers')->hourly();
     }
 
     /**
