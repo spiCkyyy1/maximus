8a7fbba6f0d8cfadb92a0fdf8ca7c634914f603e
hassam <hassam.waheed@conovoinc.com>
2021-08-03 11:00:00 +0500

    customising headers

--- app/Exports/JobSeekersExport.php
+++ app/Exports/JobSeekersExport.php
@@ -26,65 +26,86 @@ class JobSeekersExport implements FromCollection, WithHeadings, ShouldAutoSize, 
 
     public function collection()
     {
+
+        // return JobSeeker::get(['id', 'title','first_name', 'middle_name', 'last_name', 'martial_status', 'mobile', 'email', 'city',
+        //     'region', 'nin', 'dob', 'gender', 'qualification', 'full_time_employment', 'on_job_training', 'social_benficiary',
+        //     'unemployed', 'reviewed', 'status','created_at', 'updated_at','role', 'sector','job_role', 'education_major', 'education_field']);
         if($this->status == 'all')
-            return JobSeeker::get(['id', 'title','first_name', 'middle_name', 'last_name', 'martial_status', 'mobile', 'email', 'city',
-            'region', 'nin', 'dob', 'gender', 'qualification', 'full_time_employment', 'on_job_training', 'social_benficiary',
-            'unemployed', 'reviewed', 'status','created_at', 'updated_at','role', 'sector','job_role', 'education_major', 'education_field']);
+            return JobSeeker::get(['nin','first_name', 'middle_name', 'last_name', 'gender', 'dob', 'mobile','email', 'city',
+            'region', 'qualification', 'education_major', 'education_field', 'sector','role','job_role',
+            'unemployed','created_at']);
 
         if($this->status == 'selected')
-            return JobSeeker::where('status', 1)->get(['id', 'title','first_name', 'middle_name', 'last_name', 'martial_status', 'mobile', 'email', 'city',
-            'region', 'nin', 'dob', 'gender', 'qualification', 'full_time_employment', 'on_job_training', 'social_benficiary',
-            'unemployed', 'reviewed', 'status','created_at', 'updated_at','role', 'sector','job_role', 'education_major', 'education_field']);
+            return JobSeeker::where('status', 1)->get(['nin','first_name', 'middle_name', 'last_name', 'gender', 'dob', 'mobile','email', 'city',
+            'region', 'qualification', 'education_major', 'education_field', 'sector','role','job_role',
+            'unemployed','created_at']);
 
         if($this->status == 'rejected')
-            return JobSeeker::where('status', 0)->get(['id', 'title','first_name', 'middle_name', 'last_name', 'martial_status', 'mobile', 'email', 'city',
-            'region', 'nin', 'dob', 'gender', 'qualification', 'full_time_employment', 'on_job_training', 'social_benficiary',
-            'unemployed', 'reviewed', 'status','created_at', 'updated_at','role', 'sector','job_role', 'education_major', 'education_field']);
+            return JobSeeker::where('status', 0)->get(['nin','first_name', 'middle_name', 'last_name', 'gender', 'dob', 'mobile','email', 'city',
+            'region', 'qualification', 'education_major', 'education_field', 'sector','role','job_role',
+            'unemployed','created_at']);
 
         if($this->status == 'reviewed')
-            return JobSeeker::where('reviewed', 1)->get(['id', 'title','first_name', 'middle_name', 'last_name', 'martial_status', 'mobile', 'email', 'city',
-            'region', 'nin', 'dob', 'gender', 'qualification', 'full_time_employment', 'on_job_training', 'social_benficiary',
-            'unemployed', 'reviewed', 'status','created_at', 'updated_at','role', 'sector','job_role', 'education_major', 'education_field']);
+            return JobSeeker::where('reviewed', 1)->get(['nin','first_name', 'middle_name', 'last_name', 'gender', 'dob', 'mobile','email', 'city',
+            'region', 'qualification', 'education_major', 'education_field', 'sector','role','job_role',
+            'unemployed','created_at']);
     }
 
     public function headings(): array
     {
         return [
-            'ID',
-            'Title',
+            // 'ID',
+            // 'Title',
+            'NIN',
             'First Name',
             'Middle Name',
             'Last Name',
-            'Marital Status',
+            'Gender',
+            'Date of birth',
             'Mobile',
             'Email',
             'City',
             'Region',
-            'NIN',
-            'Date of birth',
-            'Gender',
-            'Qualification',
-            'Full Time Employment',
-            'On Job Training',
-            'Social Beneficiary',
-            'Un-Employed',
-            'Reviewed',
-            'Status',
-            'Created At',
-            'Updated At',
-            'Role',
-            'Sector',
-            'Job Role',
+            'Education Level',
             'Education Major',
             'Education Field',
-            // 'CV',
-            // 'Degree certificate',
-            // 'Gosi evidence',
-            'Readiness Assessment',
-            'Evaluation Assessment',
+            'Employment Sector',
+            'Role Preference',
+            'Job Role Sector',
+            'Length of Unemployment',
+            'Referral Date',
+            'Job Readiness Stream',
+            'Self Evaluation Stream',
             'Best Competencies',
             'Worst Competencies',
             'Total Competencies Answered'
+            // 'Marital Status',
+            
+            
+            
+            
+            
+            // 'Qualification',
+            // 'Full Time Employment',
+            // 'On Job Training',
+            // 'Social Beneficiary',
+            
+            // 'Reviewed',
+            // 'Status',
+            
+            // 'Updated At',
+            
+            
+            
+            
+            // 'CV',
+            // 'Degree certificate',
+            // 'Gosi evidence',
+            
+            
+            // 'Best Competencies',
+            // 'Worst Competencies',
+            // 'Total Competencies Answered'
         ];
     }
 }
